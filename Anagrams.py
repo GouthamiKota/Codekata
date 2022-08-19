e682666ddd49ed6abc9052436a7d6b00610e7e68
GouthamiKota <111335702+GouthamiKota@users.noreply.github.com>
2022-08-19 17:05:27 +0530

    Update Anagrams.py

--- Anagrams.py
+++ Anagrams.py
@@ -3,18 +3,11 @@ import gzip
 
 
 def main(wordlist_path):
-    """
-    Finds anagrams by making use of the fact that if you
-    sort the letters of two words in an anagram, then the
-    sorted letters will be the same. The sorted letters are
-    used as a key to a hashmap that keeps a list of words
-    in the anagram.
-    See: http://stackoverflow.com/questions/19600442/anagram-algorithm-using-a-hashtable-and-or-tries
-    """
+    
 
     anagrams = {}
 
-    # load all the words into a dictonary keyed by a
+    # Insert all  words into a dictonary keyed by a
     # set of the letters in the word.
     with gzip.open(wordlist_path) as wordlist:
         for word in wordlist:
